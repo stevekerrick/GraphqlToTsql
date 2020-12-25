4cfca32e169d27040fa5b67948de73f36fadcfb0
Steve Kerrick <steven.kerrick@gmail.com>
2020-12-25 15:53:21 -0600

    Dispose of writers

--- src/GraphqlToTsql/GraphqlTranslator.cs
+++ src/GraphqlToTsql/GraphqlTranslator.cs
@@ -26,39 +26,47 @@ namespace GraphqlToTsql
             var tokenStream = new CommonTokenStream(lexer);
 
             var outputSb = new StringBuilder();
-            var outputStringWriter = new StringWriter(outputSb);
-
             var errorSb = new StringBuilder();
-            var errorStringWriter = new StringWriter(errorSb);
 
-            var parser = new GqlParser(tokenStream, outputStringWriter, errorStringWriter);
-
-            // Perform the parse/translation
-            var listener = new Listener(_entityList, variableValues);
-            parser.AddParseListener(listener);
-            parser.document();
-
-            // Exit if the parse was unsuccessful
-            var errorMessage = errorSb.ToString();
-            if (!string.IsNullOrEmpty(errorMessage))
+            using (var outputStringWriter = new StringWriter(outputSb))
+            using (var errorStringWriter = new StringWriter(errorSb))
             {
-                return new TranslateResult { ParseError = errorMessage };
+                try
+                {
+                    var parser = new GqlParser(tokenStream, outputStringWriter, errorStringWriter);
+
+                    // Perform the parse/translation
+                    var listener = new Listener(_entityList, variableValues);
+                    parser.AddParseListener(listener);
+                    parser.document();
+
+                    // Exit if the parse was unsuccessful
+                    var errorMessage = errorSb.ToString();
+                    if (!string.IsNullOrEmpty(errorMessage))
+                    {
+                        return new TranslateResult { ParseError = errorMessage };
+                    }
+
+                    // We don't know how ParseOutput is used, so bail out if we see some
+                    var parseOutput = outputSb.ToString();
+                    if (!string.IsNullOrWhiteSpace(parseOutput))
+                    {
+                        throw new Exception($"Uh-oh - parser returned some Output: {parseOutput}");
+                    }
+
+                    // Parse was successful. Now perform the translation.
+                    var queryTree = listener.GetQueryTree();
+                    var builder = new TsqlBuilder();
+                    var (tsql, tsqlParameters) = builder.Build(queryTree);
+                    var result = new TranslateResult { Tsql = tsql, TsqlParameters = tsqlParameters };
+
+                    return result;
+                }
+                catch (InvalidRequestException e)
+                {
+                    return new TranslateResult { ParseError = e.Message };
+                }
             }
-
-            // We don't know how ParseOutput is used, so bail out if we see some
-            var parseOutput = outputSb.ToString();
-            if (!string.IsNullOrWhiteSpace(parseOutput))
-            {
-                throw new Exception($"Uh-oh - parser returned some Output: {parseOutput}");
-            }
-
-            // Parse was successful. Now perform the translation.
-            var queryTree = listener.GetQueryTree();
-            var builder = new TsqlBuilder();
-            var (tsql, tsqlParameters) = builder.Build(queryTree);
-            var result = new TranslateResult { Tsql = tsql, TsqlParameters = tsqlParameters };
-
-            return result;
         }
     }
 }
